88b95c6afd3c6fad3c7724a249e59a71535da312
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-06-22 13:36:41 +0000

    feat: add main onbarding page component and style

---
+++ src/page/OnBoarding/OnBoardingPage.tsx
@@ -0,0 +1,10 @@
+import './OnBoardingPage.scss';
+
+export default function () {
+    return (
+        <main>
+            <section>Section 1</section>
+            <section>Section 2</section>
+        </main>
+    );
+}
