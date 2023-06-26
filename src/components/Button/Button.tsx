2e746a3e44d1058cff20d80e02b0aefb0a2c3bf0
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-06-26 10:05:03 +0000

    refactor: change button props interface

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,13 @@
-import { HTMLProps } from 'react';
+import { ButtonHTMLAttributes } from 'react';
 import './Button.scss';
 
-interface ButtonProps extends HTMLProps<HTMLButtonElement> {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
     children: React.ReactNode;
 }
 
-export default function Button({className, children}: ButtonProps) {
+export default function Button({className, children, ...rest}: ButtonProps) {
   return (
-    <button className={`button ${className}`}>{children}</button>
+    <button className={`button ${className}`} {...rest}>{children}</button>
   )
 }
