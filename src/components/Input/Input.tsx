e36c5c9d1315e957108b477dc204388d26b372dd
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-06-26 17:14:48 +0000

    feat: implement input validation for required and type mismatch

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,4 @@
+import {  useEffect, useRef, useState } from 'react';
 import './Input.scss';
 
 interface InputProps extends React.HTMLProps<HTMLInputElement> {
@@ -12,10 +13,49 @@ export default function Input({
     labelText,
     ...rest
 }: InputProps) {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [error, setError] = useState('');
+    const [isValidating, setIsValidating] = useState(false);
+
+
+    useEffect(() => {
+        const validityState = inputRef.current?.validity;
+        if (isValidating) {
+            if (validityState?.valueMissing) {
+                setError('Field is required');
+            } else if (validityState?.typeMismatch) {
+                setError(`Please enter a valid ${inputRef.current?.type}`);
+            } else {
+                setError('');
+            }
+        }
+         else {
+            if (!error) {
+                setError('');
+            }
+        }
+    }, [isValidating, error]);
+
     return (
-        <div className="form-input">
+        <div className="input">
             {labelText && <label htmlFor={id}>{labelText}</label>}
-            <input type={type} id={id} {...rest} />
+            <input
+                type={type}
+                id={id}
+                {...rest}
+                ref={inputRef}
+                onInvalid={(e) => {
+                    e.preventDefault();
+                    setIsValidating(true);
+                }}
+                onBlur={() => {
+                    setIsValidating(true);
+                }}
+                onFocus={() => {
+                    setIsValidating(false);
+                }}
+            />
+            {error && <p>{error}</p>}
         </div>
     );
 }
