fd40f7189e6f592a4b14318502c9c47c567644fc
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-06-26 17:35:00 +0000

    feat: implement handle form submit function

--- src/template/Register/Register.tsx
+++ src/template/Register/Register.tsx
@@ -7,6 +7,7 @@ import Button from '../../components/Button/Button';
 import PageLeftSection from '../../layout/PageLeftSection';
 import PageRightSection from '../../layout/PageRightSection';
 import Navigation from '../../components/Navigation/Navigation';
+import { ChangeEvent } from 'react';
 
 interface RegisterProps {
     handleGoToNextStep: () => void;
@@ -22,6 +23,20 @@ export default function Register({
     handleGoToNextStep,
     handleGoToPreviousStep,
 }: RegisterProps) {
+
+    function handleSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const data = new FormData(e.target);
+        const fullName = data.get('fullName');
+        const email = data.get('email');
+        const password = data.get('password');
+
+        if (fullName && email && password) {
+            handleGoToNextStep();
+        }
+    }
+
     return (
         <>
             <PageLeftSection imageSrc={registerBackgroundImage} />
@@ -30,19 +45,22 @@ export default function Register({
                     {...{ steps, handleGoToPreviousStep, handleGoToNextStep }}
                 />
                 <div className="register">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <h1>Create Account</h1>
                         <Input
                             name="fullName"
                             id="fullName"
                             placeholder="John Doe"
                             labelText="Name"
+                            required
                         />
                         <Input
                             name="email"
+                            type="email"
                             id="email"
-                            placeholder="johndoe@gmail.com "
+                            placeholder="johndoe@gmail.com"
                             labelText="E-mail"
+                            required
                         />
                         <Input
                             type="password"
@@ -50,13 +68,11 @@ export default function Register({
                             id="password"
                             placeholder="***********"
                             labelText="Password"
+                            required
+                            minLength={8}
                         />
                         <div className="buttons">
-                            <Button
-                                primary
-                                type="button"
-                                onClick={handleGoToNextStep}
-                            >
+                            <Button primary type="submit">
                                 Submit
                             </Button>
                             <Button>
