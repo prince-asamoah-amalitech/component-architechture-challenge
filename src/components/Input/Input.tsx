1085c57e6eef4b81b354d0bdb42fa762d45e8504
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-06-26 09:57:29 +0000

    refactor: update prop interface

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,21 @@
 import './Input.scss';
 
-interface InputProps extends React.HTMLProps<HTMLInputElement>{
+interface InputProps extends React.HTMLProps<HTMLInputElement> {
     id: string;
     type?: string;
     labelText: string;
 }
 
-export default function Input({type = 'text', id, labelText, ...rest}: InputProps) {
+export default function Input({
+    type = 'text',
+    id,
+    labelText,
+    ...rest
+}: InputProps) {
     return (
         <div className="form-input">
-            <label htmlFor={id}>{labelText}</label>
-            <input type={type} id={id} {...rest}/>
+            {labelText && <label htmlFor={id}>{labelText}</label>}
+            <input type={type} id={id} {...rest} />
         </div>
     );
 }
