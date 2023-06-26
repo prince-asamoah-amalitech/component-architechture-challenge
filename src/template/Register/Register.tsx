7be87b45d18763bdbb11304e5b25bc1586f95ec9
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-06-26 10:14:24 +0000

    refactor: rename file and component

---
+++ src/template/Register/Register.tsx
@@ -0,0 +1,77 @@
+import './Register.scss';
+import { ReactComponent as GoogleLogo } from '../../assets/icons/google-logo.svg';
+import registerBackgroundImage from '../../assets/images/register.png';
+
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+import PageLeftSection from '../../layout/PageLeftSection';
+import PageRightSection from '../../layout/PageRightSection';
+import Navigation from '../../components/Navigation/Navigation';
+
+interface RegisterProps {
+    handleGoToNextStep: () => void;
+    handleGoToPreviousStep: () => void;
+    steps: {
+        stepsNumber: number;
+        stepsTotal: number;
+    };
+}
+
+export default function Register({
+    steps,
+    handleGoToNextStep,
+    handleGoToPreviousStep,
+}: RegisterProps) {
+    return (
+        <>
+            <PageLeftSection imageSrc={registerBackgroundImage} />
+            <PageRightSection>
+                <Navigation
+                    {...{ steps, handleGoToPreviousStep, handleGoToNextStep }}
+                />
+                <div className="register">
+                    <form>
+                        <h1>Create Account</h1>
+                        <Input
+                            name="fullName"
+                            id="fullName"
+                            placeholder="John Doe"
+                            labelText="Name"
+                        />
+                        <Input
+                            name="email"
+                            id="email"
+                            placeholder="johndoe@gmail.com "
+                            labelText="E-mail"
+                        />
+                        <Input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="***********"
+                            labelText="Password"
+                        />
+                        <div className="buttons">
+                            <Button
+                                type="button"
+                                className="primary"
+                                onClick={handleGoToNextStep}
+                            >
+                                Submit
+                            </Button>
+                            <Button className="secondary">
+                                <GoogleLogo />
+                                Sign up with Google
+                            </Button>
+                        </div>
+                        <footer>
+                            <p>
+                                Already have an account? <a href="#">Log In</a>
+                            </p>
+                        </footer>
+                    </form>
+                </div>
+            </PageRightSection>
+        </>
+    );
+}
