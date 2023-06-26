23b15b7ade5e41a96d8fe264e3268c6503c9c928
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-06-26 10:43:05 +0000

    feat: change main content component when steps state is updated

--- src/page/OnBoarding/OnBoardingPage.tsx
+++ src/page/OnBoarding/OnBoardingPage.tsx
@@ -1,13 +1,51 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import './OnBoardingPage.scss';
-import PageLeftSection from '../../template/PageLeftSection';
-import PageRightSection from '../../template/PageRightSection';
-import RegisterBackgroundImage from '../../assets/images/register.png';
 
-export default function () {
-    return (
-        <main>
-            <PageLeftSection imageSrc={RegisterBackgroundImage}/>
-            <PageRightSection/>
-        </main>
+import AddPhoto from '../../template/AddPhoto/AddPhoto';
+import Recommendations from '../../template/Recommendations/Recommendations';
+import Interest from '../../template/Interest/Interest';
+import Complete from '../../template/Complete/Complete';
+import Register from '../../template/Register/Register';
+
+const templateComponents = [
+    Register,
+    AddPhoto,
+    Recommendations,
+    Interest,
+    Complete,
+];
+
+export default function OnBoardingPage() {
+    const [stepsNumber, setStepsNumber] = useState(1);
+    const [stepsTotal] = useState(4);
+
+    const handleGoToNextStep = useCallback(() => {
+        const nextStepsNumber = stepsNumber + 1;
+        setStepsNumber(nextStepsNumber);
+    }, [stepsNumber]);
+
+    const handleGoToPreviousStep = useCallback(() => {
+        const nextStepsNumber = stepsNumber - 1;
+        setStepsNumber(nextStepsNumber);
+    }, [stepsNumber]);
+
+    const nextComponentProps = useMemo(
+        () => ({
+            steps: { stepsNumber, stepsTotal },
+            handleGoToNextStep,
+            handleGoToPreviousStep,
+        }),
+        [stepsNumber,stepsTotal, handleGoToNextStep, handleGoToPreviousStep]
     );
+
+    const [nextTemplateComponent, setNextTemplateComponent] = useState(
+        <Register {...nextComponentProps} />
+    );
+
+    useEffect(() => {
+        const Component = templateComponents[stepsNumber - 1];
+        setNextTemplateComponent(<Component {...nextComponentProps} />);
+    }, [stepsNumber, nextComponentProps]);
+
+    return <main>{nextTemplateComponent}</main>;
 }
