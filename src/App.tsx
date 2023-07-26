13de71da26fceaade7d66b7a73cd6269be9121b6
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-07-26 10:59:13 +0000

    feat: use context component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import OnBoardingPage from './page/OnBoarding/OnBoardingPage';
+import OnBoardingContext from './context/OnBoardingContext';
 
 export default function App() {
+
     return (
-        <>
+        <OnBoardingContext>
             <OnBoardingPage />
-        </>
+        </OnBoardingContext>
     );
 }
